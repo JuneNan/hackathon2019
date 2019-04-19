901f6786b0efa8785645ef01c935a68be0de0035
이준원 <junenan@ijun-won-ui-MacBookPro-2.local>
2019-04-19 23:09:16 +0900

    show_sheet sample data showing js

--- published/js/show_sheet.js
+++ published/js/show_sheet.js
@@ -3,8 +3,48 @@ $(document).ready(function(){
         var items = [];
         console.log("성공")
         $.each( data, function( key, val ) {
-           console.log(data[key].name)
+           var appendHTML = dialog_sheet(data[key],key)
+           $('.mdl-grid').append(appendHTML)
         });
     });
 })
 
+function dialog_sheet(data,i) {
+    var appendHTML = ""
+    console.log(i)
+    if (i%2 == 0 ){ 
+        appendHTML += "<div class='mdl-cell mdl-cell--2-col tc pr'>"
+        appendHTML += "<div class='profile_area'>"
+        appendHTML += "<div class='profile_picture dib' style='background-image: url("+"/img/user_profile.png"+")'></div>"
+        appendHTML += " <div class='f16 fwb tc mt5'>"
+        appendHTML += data.name
+        appendHTML += "</div>"
+        appendHTML += "</div>"
+        appendHTML += "</div>"
+    } else {
+        appendHTML +=  "<div class='mdl-cell mdl-cell--2-col'>"
+        appendHTML += "</div>"
+    }
+
+    appendHTML +=  "<div class='mdl-cell mdl-cell--8-col fit'>"
+    appendHTML +=  "<div class='announce_comment'>"
+    appendHTML += data.word
+    appendHTML += "</div>"
+    appendHTML += "</div>"
+    
+    if (i%2 == 0 ){
+        appendHTML +=  "<div class='mdl-cell mdl-cell--2-col'>"
+        appendHTML += "</div>"
+    } else {
+        appendHTML += "<div class='mdl-cell mdl-cell--2-col tc pr'>"
+        appendHTML += "<div class='profile_area'>"
+        appendHTML += "<div class='profile_picture dib' style='background-image: url("+"/img/user_profile.png"+")'></div>"
+        appendHTML += " <div class='f16 fwb tc mt5'>"
+        appendHTML += data.name
+        appendHTML += "</div>"
+        appendHTML += "</div>"
+        appendHTML += "</div>"
+    }
+    console.log(data.name)
+    return (appendHTML)
+}
